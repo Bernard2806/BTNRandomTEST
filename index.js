52136e5c86349deec2baefdbddcd157834576c61
Bernard2806 <107374580+Bernard2806@users.noreply.github.com>
2022-09-25 23:48:52 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click', function (){
+    alert('Gracias')
+});
+
+const NoBtn = document.querySelector('#NoBtn');
+
+NoBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    NoBtn.style.setProperty('top', randomY+ '%');
+    NoBtn.style.setProperty('left', randomX+ '%');
+    NoBtn.style.setProperty('transform','translate(-$(randomX)%, -$(randomY)%)');
+})
